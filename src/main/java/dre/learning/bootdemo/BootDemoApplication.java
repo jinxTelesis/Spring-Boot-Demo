4bb8dd54fafc47ea3fd7616359b167f3e90bcaf8
Dre <dremon7@hotmail.com>
2019-01-17 18:49:50 -0500

    initial commit

---
+++ src/main/java/dre/learning/bootdemo/BootDemoApplication.java
@@ -0,0 +1,16 @@
+package dre.learning.bootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootDemoApplication.class, args);
+
+
+	}
+
+}
+
